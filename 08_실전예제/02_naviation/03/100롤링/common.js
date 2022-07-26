2ca32acef046e36c3f4c6691e882a58404952131
Akuby <akuby1997@gmail.com>
2022-07-26 12:41:56 +0900

    변수를 활용한 슬라이더 문제 해결

--- 08_실전예제/02_naviation/03/100롤링/common.js
+++ 08_실전예제/02_naviation/03/100롤링/common.js
@@ -1,18 +1,60 @@
-const prevSlider = function () {
-  $('#sliderList:not(:animated)').prepend($('#sliderList li:last')).css({marginLeft:'-100%'}).animate({marginLeft:0}, 1000)
+let sList = $("#sliderList");
+let num = 0;
+let prevSlider = function () {
+  num--;
+  if (num == -1 ) {
+    num = 2;
+    sList.prepend($('.slider3')).prepend($('.slider2')) // 하나만 먼저 보내고, 콜백으로 남은 하나 보낼 수도 있다.
+    .css({
+      marginLeft : '-200%'
+    })
+    .animate({
+      marginLeft:'-100%'
+    }, 1000, function() {
+      sList.prepend($('.slider1'))
+      .css({marginLeft:'-200%'})
+    })
+  } else {
+    sList.not(":animated").animate({
+      marginLeft:"+=100%"
+    }, 1000)
+  }
 }
-const nextSlider = function () {
-  $('#sliderList:not(:animated)').animate({marginLeft:'-100%'}, 1000, function() {
-    $(this).append($('>li:first',this)).css({marginLeft:0})
-  });
-  
+let nextSlider = function () {
+  num++;
+  if (num == 3) {
+    num = 0;
+    sList.append($(".slider1")).append($(".slider2"))
+      .css({
+        marginLeft: 0
+      })
+      .animate({
+        marginLeft: "-100%"
+      }, 1000, function () {
+        sList.append($(".slider3")).css({
+          marginLeft: 0
+        })
+      })
+  } else {
+    sList.not(":animated").animate({
+      marginLeft: "-=100%"
+    }, 1000)
+  }
 }
-$('.prevBtn').on('click', function() {
+
+$(".prevBtn").on('click', function (e) {
   prevSlider();
 })
-$('.nextBtn').on('click', function() {
+$(".nextBtn").on('click', function (e) {
   nextSlider();
 })
-$('#posBtn a, #numBtn a').on('click', function(e) {
+$("#numBtn a").on('click', function (e) {
+  num = $(this).index();
+  sList.animate({
+    marginLeft: `${num*(-100)}%`
+  }), 1000
+})
+
+$("#posBtn a, #numBtn a").on('click', function (e) {
   e.preventDefault();
 })
