fd570a1f24a057c0137c1da1bef5731aa278cecd
Akuby <akuby1997@gmail.com>
2022-07-28 11:29:03 +0900

    비디오 제어

--- 09_응용예제/00_네비게이션/js/common3.js
+++ 09_응용예제/00_네비게이션/js/common3.js
@@ -5,3 +5,17 @@ $('#gnb').on('mouseenter', function() {
 $('#header').on('mouseleave',function() {
   $('#header').removeClass('on')
 })
+let onPlay = true;
+const bgVideo = document.getElementById('video')
+$("#videoBtn").on('click', function() {
+  if ( onPlay == true ){
+    bgVideo.pause(); // 제이쿼리는 비디오 선택자 사용 불가
+    $(this).text('play').css({backgroundColor:'rgba(255, 255, 255, 0.33)'})
+    onPlay = false;
+  }
+  else {
+    bgVideo.play();
+    $(this).text('stop').css({backgroundColor:'transparent'})
+    onPlay = true;
+  }
+})
