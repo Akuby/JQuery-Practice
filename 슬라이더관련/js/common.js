88284a41be46923394f1241245defa69188d1bf5
Akuby <akuby1997@gmail.com>
2022-08-03 11:18:29 +0900

    페이딩 슬라이더 완성

--- 슬라이더관련/js/common.js
+++ 슬라이더관련/js/common.js
@@ -1,31 +1,87 @@
-    /* 할 일 목록
+    let liLength = $('#sliderList li').length;
+    let classNum; // 화살표 클릭 시 번호 버튼 클래스 변경
+    let state = 1; // 빠른 클릭 방지
 
-		Tip1. position: absolute의 적용으로 보이는 순서는 아래그림부터
-
-		Tip2. 클래스 active로 인해 첫번째 그림이 보이고 있음
-
-		1. 두번째 이미지가 전환되어 보이도록 하기 위해 클래스 active 추가
-
-		2. animate()를 이용해 이펙트 적용
-
-		3. 이펙트 적용시 첫번째 그림은 클래스 #sliderList내 마지막으로 이동하고,
-			동시에 클래스 네임 삭제
-
-		*/
-
-    $('.next').on('click', function() {
-      $('#sliderList li:eq(1):not(:animated)').addClass('active').css({opacity:0}).animate({opacity:1}, 500, function() {
-        $('#sliderList').append($('#sliderList li:eq(0)')).children('li:last').removeClass('active')
-      })
+    $('.next').on('click', function () {
+      if (state == 1) {
+        state = 0;
+        nextSlider(500);
+      }
     })
-    $('.prev').on('click', function() {
-      $('#sliderList li:last:not(:animated)').addClass('active').css({opacity:0}).animate({opacity:1}, 500, function() {
-        $('#sliderList').prepend($('#sliderList li:last')).children('li:eq(1)').removeClass('active')
-      })
+    $('.prev').on('click', function () {
+      if (state == 1) {
+        state = 0;
+        prevSlider(500);
+      }
     })
 
-    $('#btnNum a').on('click', function(e) {
+    let timer = setInterval(function() {
+      nextSlider(1000)
+    }, 8000);
+
+    $('#btnNum a').on('click', function () {
+      if (state == 1) {
+        state = 0;
+        let num = $(this).index() + 1
+        if ($('.slider' + num).hasClass('active')) { // 현재 슬라이드 번호 클릭 시
+          state = 1;
+          return;
+        }
+        $('#btnNum a').removeClass('on');
+        $(this).addClass('on');
+        $(`.slider${num}`).addClass('active').css({
+          opacity: 0
+        }).animate({
+          opacity: 1
+        }, 500, function () {
+          $('#sliderList li').not($(this)).removeClass('active')
+          for (let i = 0; i < liLength - 1; i++) {
+            num++;
+            if (num == 5) {
+              num = 1
+            };
+            $('#sliderList').append($(`.slider${num}`));
+          }
+          state = 1;
+        });
+      }
+    })
+
+    $('#btnPos a, #btnNum a').on('click', function (e) {
       e.preventDefault();
-      $('#btnNum a').removeClass('on');
-      $(this).addClass('on');
+      clearInterval(timer);
+      timer = setInterval(function() {
+        nextSlider(1000)
+      }, 8000);
     })
+
+    function nextSlider (time) {
+      classNum = $('#sliderList li:first').attr('class').substr(13, 1);
+      if (classNum == liLength) {
+        classNum = 0
+      }
+      $('#btnNum a').removeClass('on');
+      $(`#btnNum a:eq(${classNum})`).addClass('on')
+      $('#sliderList li:eq(1):not(:animated)').addClass('active').css({
+        opacity: 0
+      }).animate({
+        opacity: 1
+      }, time, function () {
+        $('#sliderList').append($('#sliderList li:eq(0)')).children('li:last').removeClass('active');
+        state = 1;
+      })
+    }
+
+    function prevSlider (time) {
+      classNum = $('#sliderList li:last').attr('class').substr(13, 1) - 1;
+      $('#btnNum a').removeClass('on');
+      $(`#btnNum a:eq(${classNum})`).addClass('on')
+      $('#sliderList li:last:not(:animated)').addClass('active').css({
+        opacity: 0
+      }).animate({
+        opacity: 1
+      }, time, function () {
+        $('#sliderList').prepend($('#sliderList li:last')).children('li:eq(1)').removeClass('active');
+        state = 1;
+      })
+    }
